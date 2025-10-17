5eb5fc4ce2e1885560dc3b613a3cdeb93f5d1f77
tbkizle <tbkizle@gmail.com>
2025-10-17 18:10:29 -0400

    Add _KUMS_CONTEXT_HEADER

--- sdk/include/ndk/ketypes.h
+++ sdk/include/ndk/ketypes.h
@@ -1932,6 +1932,38 @@ typedef struct _KTHREAD
 #define NUMBER_OF_LOCK_ENTRIES 6
 #endif
 
+typedef struct _KUMS_CONTEXT_HEADER _KUMS_CONTEXT_HEADER;
+
+#ifdef _WIN64 
+#if (NTDDI_VERSION >= NTDDI_WIN7)
+struct _KUMS_CONTEXT_HEADER
+{
+    ULONGLONG P1Home;                                                       //0x0
+    ULONGLONG P2Home;                                                       //0x8
+    ULONGLONG P3Home;                                                       //0x10
+    ULONGLONG P4Home;                                                       //0x18
+    VOID* StackTop;                                                         //0x20
+    ULONGLONG StackSize;                                                    //0x28
+    ULONGLONG RspOffset;                                                    //0x30
+    ULONGLONG Rip;                                                          //0x38
+    struct _XSAVE_FORMAT* FltSave;                                          //0x40
+    union
+    {
+        struct
+        {
+            ULONGLONG Volatile:1;                                           //0x48
+            ULONGLONG Reserved:63;                                          //0x48
+        };
+        ULONGLONG Flags;                                                    //0x48
+    };
+    struct _KTRAP_FRAME* TrapFrame;                                         //0x50
+    struct _KEXCEPTION_FRAME* ExceptionFrame;                               //0x58
+    struct _KTHREAD* SourceThread;                                          //0x60
+    ULONGLONG Return;                                                       //0x68
+};
+#endif #NTDDI_VERSION >= NTDDI_WIN7
+#endif #_WIN64
+
 typedef struct _KTHREAD
 {
     DISPATCHER_HEADER Header;
